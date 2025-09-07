2b350cb63ddce42a58157d12a8ec702311af9784
KanzuNe <hieu04092007@gmail.com>
2025-09-07 14:55:44 +0700

    #

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,4 +1,40 @@
 
+from operator import le
+
+
 guest_list = ['Alice', 'Bob', 'Charlie', 'David']
 guest_list.sort(reverse=True)
 print(guest_list)
+
+gpu = ["4080 super", "5090", "1660 super", "5080"]
+gpu.reverse()
+print(gpu)
+
+valorant_agents = [
+    "Brimstone",
+    "Viper",
+    "Omen",
+    "Killjoy",
+    "Cypher",
+    "Sova",
+    "Sage",
+    "Phoenix",
+    "Jett",
+    "Reyna",
+    "Raze",
+    "Breach",
+    "Skye",
+    "Yoru",
+    "Astra",
+    "KAY/O",
+    "Chamber",
+    "Neon",
+    "Fade",
+    "Harbor",
+    "Gekko",
+    "Deadlock",
+    "Iso",
+    "Clove"
+]
+
+len(valorant_agents)
