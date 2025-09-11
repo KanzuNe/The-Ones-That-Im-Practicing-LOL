cedf32f4020eaec9f0a9e5b42874c25d4e6a8898
KanzuNe <hieu04092007@gmail.com>
2025-09-11 21:31:29 +0700

    #

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -65,6 +65,8 @@ user_names=['admin', 'user1', 'user2', 'user3', 'user4']
 for user_name in user_names:
     if user_name == 'admin':
             print("Hello admin, would you like to see a status report?")
+    elif user_name == ''
+            print("We need to find some users!")
     else:
             print(f"Hello {user_name}, thank you for loggin again!")
 
