92de0488b64faeb3d76cd417f9813b2e990ef5c6
KanzuNe <hieu04092007@gmail.com>
2025-09-11 21:21:41 +0700

    #

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -53,3 +53,9 @@ my_favoite_agents.append("Waylay")
 
 print("This is your favourite agents list:", my_favoite_agents)
 print("\nThis is your boyfriend favourite agents list:", my_bf_favourite_agents)
+
+alien_colors = ['green', 'yellow', 'red']
+if 'green' in alien_colors:
+        print("You just earned 5 points")
+else :
+        print()
