ce3106e05ce6f93d14f33c549b2f84daf749d040
KanzuNe <hieu04092007@gmail.com>
2025-10-07 10:39:39 +0700

    #

--- B25DCTN035.py
+++ B25DCTN035.py
@@ -44,7 +44,7 @@ while choice !="0":
                 gpa3 = input("Nhap gpa sinh vien 3: ")
                 sv3["gpa"]= gpa3
                 print(f"Đã thêm thành công sinh viên 3:{sv3['name']}, {sv3['grade']}, {sv3['gpa']}")
-            elif sv3["name"] == "":
+            elif sv4["name"] == "":
                 name4 = input("Nhap ten sinh vien 4:  ")
                 sv4["name"] = name4
                 grade4 = input("Nhap khoa sinh vien 4: ")
@@ -114,16 +114,16 @@ while choice !="0":
             pass
     elif choice == "4":
             print("Currently in Liet Ke Sinh Vien")
-            print("Thông tin sinh viên 1: \n")
+            print("\nThông tin sinh viên 1: ")
             for k,v in sv1.items():
                 print(f"{k.upper()}: {v}")
-            print("Thông tin sinh viên 2: \n")
+            print("\nThông tin sinh viên 2: ")
             for k,v in sv2.items():
                 print(f"{k.upper()}: {v}")
-            print("Thông tin sinh viên 3: \n")
+            print("\nThông tin sinh viên 3: ")
             for k,v in sv3.items():
                 print(f"{k.upper()}: {v}")
-            print("Thông tin sinh viên 4:")
+            print("\nThông tin sinh viên 4:")
             for k,v in sv4.items():
                 print(f"{k.upper()}: {v}")
             input("Ấn nút bất kỳ để tiếp")
@@ -135,22 +135,22 @@ while choice !="0":
                 choice1 = input("Nhập tên sinh viên: ")
                 if choice1.lower() == sv1["name".lower()]:
                     print("Đã tìm thấy sinh viên!")
-                    print("Thông tin sinh viên 1: \n")
+                    print("\nThông tin sinh viên 1: ")
                     for k,v in sv1.items():
                         print(f"{k.upper()}: {v}")
                 elif choice1.lower() == sv2["name".lower()]:
                     print("Đã tìm thấy sinh viên!")
-                    print("Thông tin sinh viên 2: \n")
+                    print("\nThông tin sinh viên 2: ")
                     for k,v in sv2.items():
                         print(f"{k.upper()}: {v}")
                 elif choice1.lower() == sv3["name".lower()]:
                     print("Đã tìm thấy sinh viên!")
-                    print("Thông tin sinh viên 3: \n")
+                    print("\nThông tin sinh viên 3: ")
                     for k,v in sv3.items():
                         print(f"{k.upper()}: {v}")
                 elif choice1.lower() == sv4["name".lower()]:
                     print("Đã tìm thấy sinh viên!")
-                    print("Thông tin sinh viên 4: \n")
+                    print("\nThông tin sinh viên 4: ")
                     for k,v in sv4.items():
                         print(f"{k.upper()}: {v}")
                     
@@ -176,26 +176,26 @@ while choice !="0":
                     print("Thông tin sinh viên 4: \n")
                     for k,v in sv4.items():
                         print(f"{k.upper()}: {v}")
-                input("Ấn nút bất kỳ để tiếp")
-                pass
+            input("Ấn nút bất kỳ để tiếp")
+            pass
     elif choice == "6":
             print("Currently in Sap Xep Sinh Vien")
             stt = int(input("Nhập kiểu sắp xếp gpa, 0 for tăng dần, 1 for giảm dần: "))
             students = [("sv1", sv1.copy()), ("sv2", sv2.copy()), ("sv3", sv3.copy()), ("sv4", sv4.copy())]
-            
+            #Fix logic sắp xếp
             if stt == 0:
                 sorted_stu = sorted(students, key=lambda stt_sv: float(stt_sv[1]["gpa"]))
                 print("Đã sắp xếp sinh viên theo GPA tăng dần!")
-               
+            
             elif stt == 1:
                 sorted_stu = sorted(students, key=lambda stt_sv: float(stt_sv[1]["gpa"]), reverse=True)
                 print("Đã sắp xếp sinh viên theo GPA giảm dần!")
-                
+            #Clear trước khi uploda data mới  
             sv1.clear()
             sv2.clear()
             sv3.clear()
             sv4.clear()
-
+            #update copy ver của dict vào org dict
             sv1.update(sorted_stu[0][1])
            
             sv2.update(sorted_stu[1][1])
@@ -210,6 +210,8 @@ while choice !="0":
         break
     else: 
         print("Sai cú pháp, nhập lại!")
+
+    os.system('cls')
 
     
 
