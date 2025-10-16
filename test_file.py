14c674267e3a6e7cc8c435001a3f3ff0d121cec0
KanzuNe <hieu04092007@gmail.com>
2025-10-16 07:55:35 +0700

    #

--- test_file.py
+++ test_file.py
@@ -1,45 +1,17 @@
-# Nhập code của bạn ở đây\
-"""
-import math
 
-def distance(x1,y1,x2,y2):
-        return math.sqrt((x2-x1)**2 + (y2-y1)**2)
-
+# Nhập code của bạn ở đây
+def solve():
     # Viết giải pháp
-T = int(input())
-
-for _ in range(T):
-    R = float(input())
-    x1,y1 = map(float, input().split())
-    x2,y2 = map(float, input().split())
-    x3,y3 = map(float, input().split())
-    d12 = distance(x1,y1,x2, y2)
-    d13 = distance(x1,y1, x3, y3)
-    d23 = distance(x2,y2, x3, y3)
-
-    res = False
-    if d12 <=R and d23<=R and d13<=R:
-        res= True
-    if d12 >= R:
-        if d13 <= R and d23 <= R:
-            res= True
+    pass
+    a = input().lower().find("chef")
     
-    if d13 >=R:
-        if d12<=R and d23<=R:
-            res = True
-    if d23 >=R:
-        if d13 <=R and d12 <= R:
-            res=True
 
-    
-    if res== True:
-            print("yes")
-    else:
-            print("no")
-"""
+            
+    print(c)
 
 
 
 
-
-    
+            
+if __name__ == "__main__":
+    solve()
